b940757ea6aa988e55b579812c861efd4d74915f
moahedyounes-debug <moahedyounes@gmail.com>
2026-02-07 19:02:13 +0300

    Refactor API request functions for clarity

--- api.js
+++ api.js
@@ -1,13 +1,22 @@
+// رابط الـ API
 const API_URL = "https://script.google.com/macros/s/AKfycbzhV1_rDRVPmbIhxyPDKx2yTTnXU0cPLBPaJ3iodITD76aK_DX0vDD-C0rx0FaNu812tQ/exec";
 
+// GET Request
 async function apiGet(params) {
-  const res = await fetch(API_URL + "?" + new URLSearchParams(params));
-  return res.json();
+    const url = API_URL + "?" + new URLSearchParams(params);
+    const res = await fetch(url);
+    return res.json();
 }
 
+// POST Request
 async function apiPost(body) {
-  const form = new FormData();
-  Object.keys(body).forEach(k => form.append(k, body[k]));
-  const res = await fetch(API_URL, { method: "POST", body: form });
-  return res.json();
+    const form = new FormData();
+    Object.keys(body).forEach(key => form.append(key, body[key]));
+
+    const res = await fetch(API_URL, {
+        method: "POST",
+        body: form
+    });
+
+    return res.json();
 }
