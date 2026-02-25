27c09ca8c0e5bde504c17b39c06f6af9d7a31b0e
moahedyounes-debug <moahedyounes@gmail.com>
2026-02-25 16:52:43 +0300

    Create translations.js

---
+++ translations.js
@@ -0,0 +1,43 @@
+const translations = {
+  ar: {
+    today: "اليوم",
+    yesterday: "أمس",
+    week: "هذا الأسبوع",
+    month: "هذا الشهر",
+    year: "هذه السنة",
+    servicesSummary: "ملخص الخدمات",
+    employeesSummary: "ملخص الموظفين",
+    completedVisits: "الزيارات المكتملة",
+    invoices: "الفواتير",
+    bookings: "الحجوزات",
+    notifications: "الإشعارات",
+  },
+
+  en: {
+    today: "Today",
+    yesterday: "Yesterday",
+    week: "This Week",
+    month: "This Month",
+    year: "This Year",
+    servicesSummary: "Services Summary",
+    employeesSummary: "Employees Summary",
+    completedVisits: "Completed Visits",
+    invoices: "Invoices",
+    bookings: "Bookings",
+    notifications: "Notifications",
+  },
+
+  ur: {
+    today: "آج",
+    yesterday: "کل",
+    week: "اس ہفتے",
+    month: "اس مہینے",
+    year: "اس سال",
+    servicesSummary: "خدمات کا خلاصہ",
+    employeesSummary: "ملازمین کا خلاصہ",
+    completedVisits: "مکمل شدہ وزٹس",
+    invoices: "رسیدیں",
+    bookings: "بکنگ",
+    notifications: "اطلاعات",
+  }
+};
